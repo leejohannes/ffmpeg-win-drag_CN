8364fdcef464670679b60a7ee0e006261c3a1e37
leejohannes <leeyoo@leeyoo.org>
2024-10-16 17:35:10 +0800

    Update 生成字幕.bat
    移动生成的wav从临时文件夹到视频所在目录
    而不是删除掉它

--- FFMPEG拖拽/生成字幕.bat
+++ FFMPEG拖拽/生成字幕.bat
@@ -4,5 +4,6 @@ setlocal enabledelayedexpansion
 title %~n1
 for /f "delims=" %%i in ('type "%~dp0config.txt"^| find /i "="') do set %%i
 %as% "%~nx1" --srt_only  "True"
-del /f %temp%\%~n1.wav
+::del /f %temp%\%~n1.wav
+move %temp%\%~n1.wav %~dp1
 ::pause
