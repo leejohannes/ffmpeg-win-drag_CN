8796a450b7381e0baefd7a6cbd21b1b494751a01
leejohannes <leeyoo@leeyoo.org>
2024-10-22 14:10:06 +0800

    Add files via upload

---
+++ FFMPEG拖拽/image2txt.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001>>nul
+setlocal enabledelayedexpansion
+for /f "delims=" %%i in ('type "%~dp0config.txt"^| find /i "="') do set %%i
+echo 请把图片摆正，二元化，剪切好以提高识别率
+title %~nx1
+%t% "%~nx1" "%~n1"
+echo.
+for /f "usebackq tokens=*" %%i in ("%~n1.txt") do (
+echo %%i
+)
+echo.
+pause
